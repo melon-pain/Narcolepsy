a5c04a441c9c9407238c31b2af0e0f6b3c106b96
melon_pain <james_omalin@dlsu.edu.ph>
2021-09-06 19:40:26 +0800

    Narcolepsy

---
+++ Assets/Scripts/Narcolepsy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Narcolepsy : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume volume;
+    private Vignette vignette;
+
+    private void Start()
+    {
+        volume.profile.TryGetSettings<Vignette>(out vignette);
+    }
+
+    public void UpdateNarcolepsy(float seconds)
+    {
+        if (seconds <= 60)
+        {
+            vignette.intensity.value = 1.0f - (seconds / 60.0f);
+        }
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,20 +5,29 @@ using UnityEngine.Events;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private int seconds = 0;
+    [SerializeField] private float minSeconds = 0;
+    [SerializeField] private float maxSeconds = 60;
+    private float seconds = 0;
+    public UnityEvent<float> OnTimerUpdate = new UnityEvent<float>();
     public UnityEvent OnTimerEnd = new UnityEvent();
+    
     private void Start()
     {
-        
+        seconds = Random.Range(minSeconds, maxSeconds);
+
+        OnTimerUpdate.Invoke(seconds);
+        StartCoroutine(Countdown());
     }
 
     private IEnumerator Countdown()
     {
         while (seconds > 0)
         {
-            seconds--;
-            yield return new WaitForSeconds(1);
+            seconds -= 0.01f;
+            OnTimerUpdate.Invoke(seconds);
+            yield return new WaitForSeconds(0.01f);
         }
         OnTimerEnd.Invoke();
+        yield break;
     }
 }
