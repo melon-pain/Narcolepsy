cfabf1015c48d467426a930ed3e425389ee2b27a
melon_pain <james_omalin@dlsu.edu.ph>
2021-09-11 22:44:58 +0800

    door

--- Assets/Scripts/Error.cs
+++ Assets/Scripts/Error.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
 [Serializable]
-[PostProcess(typeof(GrayscaleRenderer), PostProcessEvent.AfterStack, "Custom/Grayscale")]
-public sealed class Grayscale : PostProcessEffectSettings
+[PostProcess(typeof(ErrorRenderer), PostProcessEvent.AfterStack, "Custom/Grayscale")]
+public sealed class Error : PostProcessEffectSettings
 {
     [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
     public FloatParameter blend = new FloatParameter { value = 0.5f };
 }
 
-public sealed class GrayscaleRenderer : PostProcessEffectRenderer<Grayscale>
+public sealed class ErrorRenderer : PostProcessEffectRenderer<Error>
 {
     public override void Render(PostProcessRenderContext context)
     {
