326a60f31bd25f4f162a4d5863d5483e5996389d
CesarManalac <79884030+CesarManalac@users.noreply.github.com>
2021-09-14 02:38:53 +0800

    main menu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void onStart()
+    {
+        SceneManager.LoadScene("ShadScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void back(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+    public void instructions(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void onQuitPressed(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+}
